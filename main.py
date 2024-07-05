933fa98dd56c14c86b845be7e19b0371feb2e380
Tomek4861 <Tomek4861g@gmail.com>
2024-07-06 01:38:09 +0200

    Fixed app on darkmode

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-from ui.config_ui import ConfigApp
 import sys
+
 from PyQt6.QtWidgets import QApplication
+
+from ui.config_ui import ConfigApp
 
 
 def main():
-    app = QApplication(sys.argv)
+    app = QApplication(sys.argv + ['-platform', 'windows:darkmode=0'])
     window = ConfigApp()
     window.show()
     sys.exit(app.exec())
 
+
 if __name__ == "__main__":
     main()
-
-
-
-
